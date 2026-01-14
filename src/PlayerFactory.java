edd8767f51f3f28ea54e1a2948cbff4916492f8d
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-01-14 10:46:25 +0100

    update if-statement in PlayerFactory

--- src/PlayerFactory.java
+++ src/PlayerFactory.java
@@ -1,6 +1,9 @@
 public class PlayerFactory {
 
     public static Player createPlayer(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name required");
+        }
         return new Player(name);
     }
 }
