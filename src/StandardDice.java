ce5927767333c4a849a442e987f0724d6b81c524
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-01-14 16:35:18 +0100

    Dice and stanrdDice complete

--- src/StandardDice.java
+++ src/StandardDice.java
@@ -1,2 +1,21 @@
-public class StandardDice {
+import java.util.Random;
+
+public class StandardDice implements Dice {
+
+    private int sides;
+    private Random random = new Random();
+
+    public StandardDice(int sides) {
+        this.sides = sides;
+    }
+
+    @Override
+    public int roll() {
+        return random.nextInt(sides) + 1;
+    }
+
+    @Override
+    public int getSides() {
+        return sides;
+    }
 }
